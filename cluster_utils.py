6717965edec32b2300b0d1dd998a4acc6d76ceb5
IVAN WIZE <ivan.wize@gmail.com>
2023-10-22 14:49:45 -0700

    Added Documentation and Minor Restructuring

--- cluster_utils.py
+++ cluster_utils.py
@@ -2,12 +2,9 @@ import math
 
 class Circle:
     """
-    Class to represent a circle
+    Initialize a circle with center at x/y coordinates and radius r
     """
     def __init__(self, x, y, r):
-        """
-        Initialize a circle with center at x/y coordinates and radius r
-        """
         self.x = x
         self.y = y
         self.radius = r
@@ -21,23 +18,20 @@ class Circle:
 
     def overlaps(self, other):
         """
-        Check if cicles overlap
+        Find if circles overlap
         """
         return self.distance(other) <= self.radius + other.radius
 
 class Cluster:
-    """
-    Class to represent a cluster of circle
-    """
     def __init__(self, circle):
         """
-        Initialize a cluster with one circle.
+        Initialize a cluster with one circle
         """
         self.circles = [circle]
 
     def add_circle(self, circle):
         """
-        Add circle to cluster
+        Add circle to a cluster
         """
         self.circles.append(circle)
 
@@ -48,9 +42,6 @@ class Cluster:
         return any(c.overlaps(circle) for c in self.circles)
 
 def reduce_clusters(ctuple_list):
-    circles = [Circle(x, y, r) for x, y, r in ctuple_list]
-    clusters = []
-
     """
     Error handling
     """
@@ -61,24 +52,26 @@ def reduce_clusters(ctuple_list):
             raise ValueError("Each ctuple must be a tuple of three numbers.")
         if not all(isinstance(n, (int, float)) for n in ctuple):
             raise ValueError("Each element in ctuple must be a number.")
-
+    
     """
-    Reduce clusters to the circle with the maximum area in each cluster.
-
-    :param ctuple_list: A list of ctuples representing circles.
-    :return: A list of ctuples where each ctuple represents the circle with the maximum area in each cluster.
+    Reduce clusters to the circle with the maximum area in each cluster
     """
+    circles = [Circle(x, y, r) for x, y, r in ctuple_list]
+    clusters = []
+
     for circle in circles:
-        added = False # flag to check if circle has been added to any cluster
+        added = False
         for cluster in clusters:
             if cluster.has_overlap(circle):
                 cluster.add_circle(circle)
                 added = True
                 break
-        if not added: # if the circle doesn't overlap with any existing clusters, create a new cluster with that circle
+        if not added:
             clusters.append(Cluster(circle))
+    
     reduced_clusters = []
     for cluster in clusters:
         max_area_circle = max(cluster.circles, key=lambda c: c.area)
         reduced_clusters.append((max_area_circle.x, max_area_circle.y, max_area_circle.radius))
+    
     return reduced_clusters
